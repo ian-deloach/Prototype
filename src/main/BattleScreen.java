9f64cc6c820c02db5d583e67c02caa4e483d3b24
Ian DeLoach <iandeloach398@gmail.com>
2025-11-29 12:44:29 -0600

    Read in die from file

--- src/main/BattleScreen.java
+++ src/main/BattleScreen.java
@@ -10,35 +10,35 @@ import java.util.HashMap;
 
 public class BattleScreen {
 
-    BufferedImage draftDie1;
-    BufferedImage draftDie2;
-    BufferedImage draftDie3;
-    BufferedImage draftDie4;
-    BufferedImage draftDie5;
-    BufferedImage draftDie6;
+    HashMap<String, BufferedImage> dieImages = new HashMap<>();
     HashMap<BufferedImage, Point> images = new HashMap<>();
     DrawPanel dicePanel = new DrawPanel();
 
     public void createDice(GameScreen screen) throws IOException {
-        draftDie1 = ImageIO.read(new File("src/resources/d6.png"));
-        draftDie2 = ImageIO.read(new File("src/resources/d6.png"));
-        draftDie3 = ImageIO.read(new File("src/resources/d6.png"));
-        draftDie4 = ImageIO.read(new File("src/resources/d6.png"));
-        draftDie5 = ImageIO.read(new File("src/resources/d6.png"));
-        draftDie6 = ImageIO.read(new File("src/resources/d6.png"));
-        images.put(draftDie1, new Point(100, 100));
-        images.put(draftDie2, new Point(200, 100));
-        images.put(draftDie3, new Point(300, 100));
-        images.put(draftDie4, new Point(400, 100));
-        images.put(draftDie5, new Point(500, 100));
-        images.put(draftDie6, new Point(600, 100));
+        loadDice();
+
+        images.put(dieImages.get("d1.png"), new Point(100, 100));
+        images.put(dieImages.get("d2.png"), new Point(200, 100));
+        images.put(dieImages.get("d3.png"), new Point(300, 100));
+        images.put(dieImages.get("d4.png"), new Point(400, 100));
+        images.put(dieImages.get("d5.png"), new Point(500, 100));
+        images.put(dieImages.get("d6.png"), new Point(600, 100));
         dicePanel.setOpaque(false);
         dicePanel.setBounds(0, 0, 1280, 720);
 
         screen.getLayers().add(dicePanel, 1);
         screen.repaint();
         dicePanel.setBounds(0, 0, screen.getWidth(), screen.getHeight());
+    }
 
+    public void loadDice() throws IOException {
+        File folder = new File("src/resources/dice/");
+        File[] loadedImages = folder.listFiles();
+
+        assert loadedImages != null;
+        for (File file : loadedImages) {
+            dieImages.put(file.getName(), ImageIO.read(file));
+        }
     }
 
     private class DrawPanel extends JPanel {
