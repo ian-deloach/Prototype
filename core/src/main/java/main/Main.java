b1410172f2f71cbd89c4f7ccf1c75e69578604e5
ian-deloach <iandeloach398@gmail.com>
2026-05-10 23:27:42 -0500

    Merged old code into new framework

---
+++ core/src/main/java/main/Main.java
@@ -0,0 +1,47 @@
+package main;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import entity.Battle;
+import creatures.Enemy;
+import creatures.Player;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.logging.LogManager;
+
+public class Main extends ApplicationAdapter {
+
+    public static void main(String[] args) throws IOException {
+        Player player = new Player();
+        Enemy enemy = new Enemy();
+        Battle battle = new Battle(player, enemy);
+
+        battle.runBattle();
+
+    }
+    private SpriteBatch batch;
+    private Texture image;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        image = new Texture("libgdx.png");
+    }
+
+    @Override
+    public void render() {
+        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        batch.begin();
+        batch.draw(image, 140, 210);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        image.dispose();
+    }
+}
